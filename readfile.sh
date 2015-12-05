7aa532e3b2b0a92243c2812c6bbcbc3494a18acc
jack <jack@www>
2015-12-05 11:45:08 +0800

    shell to read file with no eof ended

---
+++ readfile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo $#
+if [ "$#" -eq 0 ];then 
+    while read line; do
+        echo $line
+    done < ./$0 
+else
+    while read line || [[ -n ${line} ]]; do
+        echo $line
+    done < ./$0 
+fi
+echo "no eof line"
